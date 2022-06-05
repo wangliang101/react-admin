93568ce5a5d85f7ca2bfdcd6218b54ff7046b847
wangliang <wangliang4468@126.com>
2022-06-05 19:19:45 +0800

    init

---
+++ admin-client_final_redux/src/pages/order/order.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './order.less'
+
+/*
+Home路由组件
+ */
+export default function Order(props) {
+  return (
+    <div className="order">
+      订单管理模块开发中...
+    </div>
+  )
+}
